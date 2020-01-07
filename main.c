57238a0fed80db9fe73c3ad6cf2615a8afd176c7
Dor Getter <dorgetter123@gmail.com>
2020-01-07 22:30:30 +0200

    done

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 // define character size
 #define CHAR_SIZE 26
+#include <stdio.h>
 
 // A Trie node
 struct Trie
 {
     int isLeaf;	// 1 when node is a leaf node
     struct Trie* character[CHAR_SIZE];
+    int counter;
 };
 
 // Function that returns a new Trie node
@@ -15,7 +16,7 @@ struct Trie* getNewTrieNode()
 {
     struct Trie* node = (struct Trie*)malloc(sizeof(struct Trie));
     node->isLeaf = 0;
-
+    node->counter=0;
     for (int i = 0; i < CHAR_SIZE; i++)
         node->character[i] = NULL;
 
@@ -32,106 +33,15 @@ void insert(struct Trie *head, char* str)
         // create a new node if path doesn't exists
         if (curr->character[*str - 'a'] == NULL)
             curr->character[*str - 'a'] = getNewTrieNode();
-
         // go to next node
         curr = curr->character[*str - 'a'];
-
         // move to next character
         str++;
-    }
-
+    } curr->counter++;
     // mark current node as leaf
     curr->isLeaf = 1;
 }
 
-// Iterative function to search a string in Trie. It returns 1
-// if the string is found in the Trie, else it returns 0
-int search(struct Trie* head, char* str)
-{
-    // return 0 if Trie is empty
-    if (head == NULL)
-        return 0;
-
-    struct Trie* curr = head;
-    while (*str)
-    {
-        // go to next node
-        curr = curr->character[*str - 'a'];
-
-        // if string is invalid (reached end of path in Trie)
-        if (curr == NULL)
-            return 0;
-
-        // move to next character
-        str++;
-    }
-
-    // if current node is a leaf and we have reached the
-    // end of the string, return 1
-    return curr->isLeaf;
-}
-
-// returns 1 if given node has any children
-int haveChildren(struct Trie* curr)
-{
-    for (int i = 0; i < CHAR_SIZE; i++)
-        if (curr->character[i])
-            return 1;	// child found
-
-    return 0;
-}
-
-// Recursive function to delete a string in Trie
-int deletion(struct Trie **curr, char* str)
-{
-    // return if Trie is empty
-    if (*curr == NULL)
-        return 0;
-
-    // if we have not reached the end of the string
-    if (*str)
-    {
-        // recur for the node corresponding to next character in
-        // the string and if it returns 1, delete current node
-        // (if it is non-leaf)
-        if (*curr != NULL && (*curr)->character[*str - 'a'] != NULL &&
-            deletion(&((*curr)->character[*str - 'a']), str + 1) &&
-            (*curr)->isLeaf == 0)
-        {
-            if (!haveChildren(*curr))
-            {
-                free(*curr);
-                (*curr) = NULL;
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-    }
-
-    // if we have reached the end of the string
-    if (*str == '\0' && (*curr)->isLeaf)
-    {
-        // if current node is a leaf node and don't have any children
-        if (!haveChildren(*curr))
-        {
-            free(*curr); // delete current node
-            (*curr) = NULL;
-            return 1; // delete non-leaf parent nodes
-        }
-
-            // if current node is a leaf node and have children
-        else
-        {
-            // mark current node as non-leaf node (DON'T DELETE IT)
-            (*curr)->isLeaf = 0;
-            return 0;	   // don't delete its parent nodes
-        }
-    }
-
-    return 0;
-}
 
 void build_word(char input[], int start, int end,struct Trie* head){
 
@@ -162,24 +72,40 @@ void appendtoinput(char *input, struct Trie* head){
         }else if(temp>= 65 && temp<=90){
             temp = temp+32;
             input[i] = temp;
-        } else if(temp == 44){
+        } else if(temp == 32){
             build_word(input,j,i,head);
             j = i+1;
         }
         i++;
 
         if(i == sizeof(&input)/ sizeof(char)){
-            input = (char *)realloc(input, sizeof(input)+10);
+            input = (char *)realloc(input, sizeof(input)*2);
         }
     }
     build_word(input,j,i,head);
     free(input);
 }
 
-void display(struct Trie* root, char str[], int level)
+void delete(struct Trie* root)
 {
+
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        if (root->character[i])
+        {
+            delete(root->character[i]);
+        }
+    }
+    free(root);
+
+}
+
+
+void display(struct Trie* root, char str[], int level,struct Trie* r)
+{
+
     if(level > sizeof(&str)){
-        str =(char*) realloc(str, sizeof(&str)+1);
+        str =(char*) realloc(str, sizeof(&str)*2);
     }
     // If node is leaf node, it indicates end
     // of string, so a null character is added
@@ -187,8 +113,7 @@ void display(struct Trie* root, char str[], int level)
     if (root->isLeaf)
     {
         str[level] = '\0';
-        printf("%s",str);
-        free(str);
+        printf("%s\t%d\n",str,root->counter);
     }
 
     int i;
@@ -201,74 +126,73 @@ void display(struct Trie* root, char str[], int level)
         if (root->character[i])
         {
             str[level] = i + 'a';
-            display(root->character[i], str, level + 1);
+            display(root->character[i], str, level + 1,r);
         }
     }
+
+}
+
+void displayrev(struct Trie* root, char str[], int level)
+{
+
+    if(level > sizeof(&str)){
+        str =(char*) realloc(str, sizeof(&str)*2);
+    }
+    // If node is leaf node, it indicates end
+    // of string, so a null character is added
+    // and string is displayed
+    if (root->isLeaf)
+    {
+        str[level] = '\0';
+        printf("%s\t%d\n",str,root->counter);
+    }
+
+    int i;
+    for (i = CHAR_SIZE-1; i >= 0; i--)
+    {
+        // if NON NULL child is found
+        // add parent key to str and
+        // call the display function recursively
+        // for child node
+        if (root->character[i])
+        {
+            str[level] = i + 'a';
+            displayrev(root->character[i], str, level + 1);
+        }
+    }
+
+}
+
+void disdis(struct Trie* root, char str[], int level){
+    display(root,str,level,root);
+    free(str);
+}
+
+void disdisrev(struct Trie* root, char str[], int level){
+    displayrev(root,str,level);
+    free(str);
 }
 
 
 // Trie Implementation in C - Insertion, Searching and Deletion
 int main(int argc, char **argv)
 {
-    int rev = 0;
-
-    if(argc == 2){
-        if(argv[1][0] == 'r' ){
-            rev =1;
-        }
-    }
     int temp;
     scanf("%d",&temp);
 
-    char *input = (char*)malloc(sizeof(char)*100);
-
+    char *input = (char*)malloc(sizeof(char)*10);
     struct Trie* head = getNewTrieNode();
 
     appendtoinput(input, head);
 
-    char *str = (char*)malloc(sizeof(char)*100);
-    display(head,str,0);
-
-
-//    insert(head, "hello");
-//
-//
-//
-//
-//    printf("%d ", search(head, "hello"));   	// print 1
-//
-//    insert(head, "helloworld");
-//    printf("%d ", search(head, "helloworld"));  // print 1
-//
-//    printf("%d ", search(head, "helll"));   	// print 0 (Not present)
-//
-//    insert(head, "hell");
-//    printf("%d ", search(head, "hell"));		// print 1
-//
-//    insert(head, "h");
-//    printf("%d \n", search(head, "h")); 		// print 1 + newline
-//
-//    deletion(&head, "hello");
-//    printf("%d ", search(head, "hello"));   	// print 0 (hello deleted)
-//    printf("%d ", search(head, "helloworld"));  // print 1
-//    printf("%d \n", search(head, "hell"));  	// print 1 + newline
-//
-//    deletion(&head, "h");
-//    printf("%d ", search(head, "h"));   		// print 0 (h deleted)
-//    printf("%d ", search(head, "hell"));		// print 1
-//    printf("%d\n", search(head, "helloworld")); // print 1 + newline
-//
-//    deletion(&head, "helloworld");
-//    printf("%d ", search(head, "helloworld"));  // print 0
-//    printf("%d ", search(head, "hell"));		// print 1
-//
-//    deletion(&head, "hell");
-//    printf("%d\n", search(head, "hell"));   	// print 0 + newline
-//
-//    if (head == NULL)
-//        printf("Trie empty!!\n");   			// Trie is empty now
-//
-//    printf("%d ", search(head, "hell"));		// print 0
+    if(argc == 2 && argv[1][0] == 'r' ){
+        char *str1 = (char*)malloc(sizeof(char)*10);
+        disdisrev(head,str1,0);
+    } else{
+        char *str = (char*)malloc(sizeof(char)*10);
+        disdis(head,str,0);
+    }
+    delete(head);
 
     return 0;
 }
